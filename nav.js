85461000ce89f0a8e8b8dfaa6fb66cefadf75ba3
Mischa Larkins <MischaLarkins06@gmail.com>
2024-04-23 05:23:38 -0700

    1.0.0

---
+++ nav.js
@@ -0,0 +1,10 @@
+document.write(`
+
+    <img id="logo" src="assets/test_logo.png" />
+    <a href=""> Page 1 </h1>
+    <a href=""> Page 2 </a>
+    <a href=""> Page 3 </a>
+    <a href=""> Page 4 </a>
+    <a href=""> Page 5 </a>
+
+`);
